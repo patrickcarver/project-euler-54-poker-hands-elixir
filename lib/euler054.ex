939112ccca1b6d0cf609477b359197ffde9c2f1a
Patrick Carver <patrickcarver@gmail.com>
2018-02-24 16:39:35 -0600

    Create project

---
+++ lib/euler054.ex
@@ -0,0 +1,18 @@
+defmodule Euler054 do
+  @moduledoc """
+  Documentation for Euler054.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Euler054.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/euler054_test.exs
@@ -0,0 +1,8 @@
+defmodule Euler054Test do
+  use ExUnit.Case
+  doctest Euler054
+
+  test "greets the world" do
+    assert Euler054.hello() == :world
+  end
+end
