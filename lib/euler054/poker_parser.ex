8e63b605cb799e1f88be925b6365e7fe5d896aa1
Patrick Carver <patrickcarver@gmail.com>
2018-02-24 19:48:38 -0600

    Split lines into lits on space

--- lib/euler054/poker_parser.ex
+++ lib/euler054/poker_parser.ex
@@ -1,7 +1,16 @@
 defmodule Euler054.PokerParser do
     def start(text_file) do
+        player1_wins = 0
+
+        text_file
+        |> clean_data
+    end
+
+    defp clean_data(text_file) do
         "../../assets/" <> text_file
         |> Path.expand(__DIR__)
         |> File.read!
+        |> String.split(~r/\n/)
+        |> Enum.map(&(String.split(&1, " "))) 
     end
 end
