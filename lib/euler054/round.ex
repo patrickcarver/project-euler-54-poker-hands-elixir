c151dc8712546880d8e9db45410b6ec72ec8f890
Patrick Carver <patrickcarver@gmail.com>
2018-02-24 21:11:12 -0600

    Sort cards in hands by value

--- lib/euler054/round.ex
+++ lib/euler054/round.ex
@@ -3,15 +3,17 @@ defmodule Euler054.Round do
     alias Euler054.Card
 
     def create_from_text(text) do
-        {player1, player2} = Enum.split(text, 5)
+        {player1_list, player2_list} = Enum.split(text, 5)
 
-       # player1 = create_hand(Enum.slice(text, 0, 5))
-       # player2 = create_hand(Enum.slice(text, 5, 5))
+        player1 = create_hand(player1_list)
+        player2 = create_hand(player2_list)
 
         player1
+        
     end
 
     defp create_hand(list) do
         Enum.map(list, &Card.create_from_text/1)
+        |> Enum.sort_by(&(&1.value))
     end
 end
