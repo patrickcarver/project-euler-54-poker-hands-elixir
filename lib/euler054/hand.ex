3bc7c442ae113ae6f118092884a8ea7369c920bc
Patrick Carver <patrickcarver@gmail.com>
2018-02-24 22:16:23 -0600

    Create Hand module

---
+++ lib/euler054/hand.ex
@@ -0,0 +1,5 @@
+defmodule Euler054.Hand do
+    def get_value(cards) do
+        cards
+    end
+end
--- lib/euler054/round.ex
+++ lib/euler054/round.ex
@@ -1,19 +1,22 @@
 defmodule Euler054.Round do
     alias __MODULE__
-    alias Euler054.Card
+    alias Euler054.{Card, Hand}
 
     def create_from_text(text) do
-        {player1_list, player2_list} = Enum.split(text, 5)
+        {hand1_list, hand2_list} = Enum.split(text, 5)
 
-        player1 = create_hand(player1_list)
-        player2 = create_hand(player2_list)
-
-        player1
-        
+        hand1 = get_hand_value(hand1_list)
+        hand2 = get_hand_value(hand2_list)
     end
 
     defp create_hand(list) do
         Enum.map(list, &Card.create_from_text/1)
         |> Enum.sort_by(&(&1.value))
+    end
+
+    defp get_hand_value(list) do
+        list
+        |> create_hand
+        |> Hand.get_value
     end
 end
